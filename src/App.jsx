e1e15c9fb4f4aa5d9202b9c9be3ce22df4464a0d
madhushathennakoon <madhushasampath@gmail.com>
2024-06-04 10:43:43 +0530

    implementing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <h1 className="text-x1 font-bold text-red-500 text-center">
-      Hello React Vite
-    </h1>
+    <div className="overflow-x-hidden">
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Navlinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 1,
+    name: "Create New Restaurent",
+    link: "/#",
+  },
+  {
+    id: 1,
+    name: "Contact",
+    link: "/#",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-white shadow-md">
+        <div className="container flex justify-between py-4 sm:py-3 ">
+          {/* Logo section */}
+          <div className="font-bold text-3xl ">Restaurants Service </div>
+          {/* Navlinks section */}
+          <div>
+            <ul className="flex items-center gap-10">
+              {Navlinks.map(({ id, name, link }) => (
+                <li key={id}>
+                  <a
+                    href={link}
+                    className="inline-block hover:text-primary text-xl font-semibold"
+                  >
+                    {name}
+                  </a>
+                </li>
+              ))}
+              {/* Login button section */}
+              <li>
+                <button className="flex justify-center items-center gap-2 bg-secondary text-xl h-[40px] text-white px-5 py-2 hover:scale-105">
+                  LOGIN
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
